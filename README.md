42c306b8ad88fd743febf888b16c57605d9f58c2
Julian <Van Ost>
2020-02-22 16:17:23 -0800

    Final commit to github

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # Team Profile Generator
 Node.js and Constructor based team or employee profile generator with unit testing.
+Handlebars, Jest, and Inquirer are required package installs.
+
+<a href="https://github.com/julianvanost" style="float:right"><img src="https://avatars3.githubusercontent.com/u/13158626?s=460&v=4" alt="Julian Van Ost" title="Julian Van Ost" width="120" height="120"></a>
+
+# Team Profile Generator
+
+![License: None](https://img.shields.io/badge/License-None-brightgreen)
+
+__Team Profile Generator__
+Dynamically generates your team's profile based on terminal input via inquirer.
+
+__Installation:__
+npm install inquirer
+npm install handlebars
+npm install jest
+npm install babel
+
+__Usage:__
+Run: node app.js
+
+__Contributors:__
+Julian Van Ost
+
+__Tests:__
+Constructor: Employee.js Passed
+Constructor: Engineer.js Passed
+Constructor: Intern.js Passed
+Constructor: Manager.js Passed
+
+__Readout:__
+jest --verbose test/*
+
+ PASS  test/Intern.test.js  
+  √ Can set school via constructor (3ms)
+  √ getRole() should return "Intern"
+  √ Can get school via getSchool()
+
+ PASS  test/Engineer.test.js
+  √ Can set GitHUb account via constructor (2ms)
+  √ getRole() should return "Engineer"
+  √ Can get GitHub username via getGithub()
+
+ PASS  test/Manager.test.js
+  √ Can set office number via constructor argument (3ms)
+  √ getRole() should return "Manager"
+  √ Can get office number via getOffice() (1ms)
+
+ PASS  test/Employee.test.js
+  √ Can instantiate Employee instance (3ms)
+  √ Can set name via constructor arguments (1ms)
+  √ Can set id via constructor argument
+  √ Can set email via constructor argument (1ms)
+  √ Can get name via getName()
+  √ Can get id via getId() (1ms)
+  √ Can get email via getEmail()
+  √ getRole() should return "Employee" (1ms)
+
+Test Suites: 4 passed, 4 total
+Tests:       17 passed, 17 total
+Snapshots:   0 total
+Time:        2.588s
+
+__Questions:__
+None
+
+
+_Repo by julianvanost_
--- app.js
+++ app.js
@@ -1,156 +1,188 @@
 const prompt = require('inquirer').createPromptModule()
-const express = require('express')
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
 const fs = require('fs')
-const path = require('path')
+const Handlebars = require('handlebars')
+let teamMembersArr = []
+let gHTML = ''
 
-function DigitalPal(hungry, sleepy, bored, age, outside, houseCondition) {
-  this.hungry = hungry
-  this.sleepy = sleepy
-  this.bored = bored
-  this.age = age
-  this.outside = outside
-  this.houseCondition = houseCondition
-  this.feed = function () {
-    if (this.hungry) {
-      console.log('That was yummy!')
-      this.hungry = false
-      this.sleepy = true
-    } else {
-      console.log(`No thanks! I'm full.`)
-    }
+let teamNumQues = [
+  'How many total team members are there?'
+]
+
+let managerQuestions = [
+  'Please enter the name of the manager?',
+  'What is the office number?'
+]
+
+let engineerQuestions = [
+  'What is your gitHub? '
+]
+
+let internQuestions = [
+  'What is the school that you attend?'
+]
+
+let questions = [
+  'What is your name?',
+  'What is id?',
+  'What is your email?',
+  'What is your role?'
+]
+
+async function startQuestions() {
+  const initPrompt = await prompt([{
+    type: 'input',
+    name: 'teamNumber',
+    message: teamNumQues[0]
+  },
+  {
+    type: 'input',
+    name: 'managerName',
+    message: managerQuestions[0]
+  },
+  {
+    type: 'input',
+    name: 'managerOffice',
+    message: managerQuestions[1]
+  },
+  {
+    type: 'input',
+    name: 'managerId',
+    message: questions[1]
+  },
+  {
+    type: 'input',
+    name: 'managerEmail',
+    message: questions[2]
   }
-  this.sleep = function () {
-    if (this.sleepy) {
-      console.log('Zzzzzzzz')
-      this.sleepy = false
-      this.bored = true
-      this.increaseAge()
-    } else {
-      console.log(`No way! I'm not tired.`)
-    }
-  }
-  this.play = function () {
-    if (this.bored) {
-      console.log(`Yay! Let's play!`)
-      this.bored = false
-      this.hungry = true
-    } else {
-      console.log('Not right now. Later?')
-    }
-  }
-  this.increaseAge = function () {
-    this.age = this.age + 1
-    console.log(`Happy Birthday to me! I am ${this.age} years old!`)
-  }
-  this.bark = function () {
-    console.log('Woof! Woof!')
-  }
-  this.goOutside = function () {
-    if (!this.outside) {
-      console.log('Yay! I love the outdoors!')
-      this.outside = true
-      this.bark()
-    } else {
-      console.log(`We're already outside though...`)
-    }
-  }
-  this.goInside = function () {
-    if (this.outside) {
-      console.log('Do we have to? Fine...')
-      this.outside = false
-    } else {
-      console.log(`I'm already inside...`)
-    }
-  }
-  this.meow = function () {
-    console.log('Meow! Meow!')
-  }
-  this.destroyFurniture = function () {
-    if (this.houseCondition > 0) {
-      this.houseCondition = this.houseCondition - 10
-      console.log('MUAHAHAHAHA! TAKE THAT FURNITURE!')
-      this.bored = false
-      this.sleepy = true
-    }
-  }
-  this.buyNewFurniture = function () {
-    this.houseCondition = this.houseCondition + 50
-    console.log('Are you sure about that?')
-  }
+  ])
+    .then(({ teamNumber, managerName, managerOffice, managerId, managerEmail }) => {
+      teamNumber = teamNumber
+      let lead = new Manager(managerName, managerId, managerEmail, managerOffice)
+      teamMembersArr.push(lead)
+      askTeamMembers(teamNumber - 1)
+
+
+    })
+    .catch(e => console.error(e))
 }
 
-const dog = new DigitalPal(true, false, true, 12, false, 100)
-const cat = new DigitalPal(false, true, false, 9, true, 100)
-dog.feed()
-dog.sleep()
-cat.sleep()
+async function askTeamMembers(teamNumber) {
+  for (let i = 0; i < teamNumber; i++) {
+    let currentRole
+    let currentName
+
+    const internEngineer = await prompt([{
+      type: 'input',
+      name: `memberName`,
+      message: questions[0]
+    },
+    {
+      type: 'list',
+      name: `memberRole`,
+      message: questions[3],
+      choices: ["Engineer", "Intern"]
+    }])
+      .then(({ memberName, memberRole }) => {
+        currentName = memberName
+        currentRole = memberRole
+      })
+
+    if (currentRole === 'Engineer') {
+      const engineerPrompt = await prompt([{
+        type: 'input',
+        name: `memberId`,
+        message: questions[1]
+      }, {
+        type: 'input',
+        name: `memberEmail`,
+        message: questions[2]
+      },
+      {
+        type: 'input',
+        name: `memberGithub`,
+        message: engineerQuestions[0]
+      }
+      ]).then(({ memberId, memberEmail, memberGithub }) => {
+        let engineerMember = new Engineer(currentName, memberId, memberEmail, memberGithub)
+        teamMembersArr.push(engineerMember)
+      }
+      )
+    }
+    else {
+      const internPrompt = await prompt([{
+        type: 'input',
+        name: `memberId`,
+        message: questions[1]
+      }, {
+        type: 'input',
+        name: `memberEmail`,
+        message: questions[2]
+      },
+      {
+        type: 'input',
+        name: `memberSchool`,
+        message: internQuestions[0]
+      }
+      ]).then(({ memberId, memberEmail, memberSchool }) => {
+
+        let internMember = new Intern(currentName, memberId,
+          memberEmail, memberSchool)
+
+        teamMembersArr.push(internMember)
+      }
+      )
+
+    }
+  }
 
 
-// let conetstant = ''
-// let panelists = []
-// let questions = ['What\'s my favorite food dish?', 'If I had a snake, what would I name it?', 'Guess my grandmother\'s name.', 'Guess my age!', 'how much u luv me?']
-// let panelistPoints = []
-// let numOfPanelists
-// async function getUserInput(message, type) {
-//   try {
-//     const { response } = await prompt({
-//       type: type,
-//       name: 'response',
-//       message: message
-//     })
-//     return response
-//   } catch (e) { console.log(e) }
-// }
-// function validateAnswers(answer, panAnswers) {
-//   console.log(`${conetstant}'s Answer: ${answer}`)
-//   for (let i = 0; i < panAnswers.length; i++) {
-//     console.log(`${panelists[i].name} guessed: ${panAnswers[i]}`)
-//     if (answer === panAnswers[i]) {
-//       panelists[i].points++
-//     }
-//   }
-// }
-// function sortPanelistsPoints() {
-//   panelists.sort((a, b) => {
-//     if (a.points > b.points) {
-//       return -1
-//     } else if (a.points < b.points) {
-//       return 1
-//     }
-//     return 0
-//   })
-// }
-// function runEndOfGame() {
-//   let matches = 0
-//   for (let i = 0; i < panelists.length; i++) {
-//     if (panelists[i].points === questions.length) {
-//       matches++
-//     }
-//   }
-//   console.log(`${conetstant} had ${matches}!`)
-//   for (let i = 0; i < panelists.length; i++) {
-//     console.log(`${panelists[i].name} has ${panelists[i].points} points!`)
-//   }
-// }
-// async function runMatchGame() {
-//   try {
-//     conetstant = await getUserInput('Please enter contestants name:', 'input')
-//     numOfPanelists = await getUserInput('Please enter number of panelists:', 'number')
-//     for (let i = 0; i < numOfPanelists; i++) {
-//       let pan = await getUserInput(`Please enter pnaelist #${i + 1}'s name:`, 'input')
-//       panelists.push({ name: pan, points: 0 })
-//     }
-//     for (let i = 0; i < questions.length; i++) {
-//       const answer = await getUserInput(`${questions[i]}\nContestants Answer:`, 'password')
-//       let panAnswers = []
-//       for (let j = 0; j < panelists.length; j++) {
-//         panAnswers.push(await getUserInput(`Panelist #${j + 1} (${panelists[j].name}) Answer:`, 'password'))
-//       }
-//       validateAnswers(answer, panAnswers)
-//     }
-//     sortPanelistsPoints()
-//     runEndOfGame()
-//   } catch (e) {
-//   }
-// }
-// runMatchGame()
+  // display each team member
+  let htmlData = ''
+
+  teamMembersArr.forEach((elem, i) => {
+    if (elem instanceof Manager) {
+      let managerSrc = readTemplateFile('./templates/manager.html')
+      let managerTemplate = Handlebars.compile(managerSrc)
+      let resultManager = managerTemplate(elem);
+      htmlData += resultManager
+    }
+    else if (elem instanceof Intern) {
+      let InternSrc = readTemplateFile('./templates/intern.html')
+      let InternTemplate = Handlebars.compile(InternSrc)
+      let resultIntern = InternTemplate(elem);
+      htmlData += resultIntern
+    }
+    else {
+      let EngineerSrc = readTemplateFile('./templates/engineer.html')
+      let EngineerTemplate = Handlebars.compile(EngineerSrc);
+      let resultEngineer = EngineerTemplate(elem);
+      htmlData += resultEngineer
+
+    }
+
+  })
+
+  let mainSrc = readTemplateFile('./templates/main.html')
+  let mainTemplate = Handlebars.compile(mainSrc);
+  let data = { data: htmlData }
+  let resultMain = mainTemplate(data);
+
+  writeToFile('./output/Team.html', resultMain)
+}
+
+const readTemplateFile = (fileName) => {
+  return fs.readFileSync(fileName, 'utf8');
+}
+
+const writeToFile = (fileName, data) => {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+}
+
+startQuestions()
